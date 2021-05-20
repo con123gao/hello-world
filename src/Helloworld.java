13600f10aba5ddc79d9926d9b2c511af4e2e11d6
shijigao <1748272380@qq.com>
2021-05-20 20:20:56 +0800

    我的一个Git

---
+++ src/Helloworld.java
@@ -0,0 +1,12 @@
+/**
+ * @author shijigao
+ * @create 2021-05-20 20:08
+ * @purpose:
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        int i=1;
+        i++;
+        System.out.println(i);
+    }
+}
